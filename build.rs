0a91057afd7bf99a6b5a23ff389cddfa29cee5a9
Varkaria <61514399+HakuraSukinoe@users.noreply.github.com>
2020-05-08 19:45:52 +0700

    remove this logo because it's akat
    ya

--- build.rs
+++ build.rs
@@ -9,8 +9,6 @@ fn install_manifest() {
     #[cfg(windows)]
     {
         let mut res = winres::WindowsResource::new();
-
-        res.set_icon("resources/icon.ico");
     
         res.compile().unwrap();
     }
