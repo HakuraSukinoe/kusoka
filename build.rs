ef89df0e54c9f33517fbb9597178b58619553b36
Varkaria <61514399+HakuraSukinoe@users.noreply.github.com>
2020-05-08 21:59:46 +0700

    add icon

--- build.rs
+++ build.rs
@@ -9,6 +9,8 @@ fn install_manifest() {
     #[cfg(windows)]
     {
         let mut res = winres::WindowsResource::new();
+
+        res.set_icon("resources/icon.ico");
     
         res.compile().unwrap();
     }
